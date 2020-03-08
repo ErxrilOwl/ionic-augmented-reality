495f22dd11a535f8832c6115dfca30f2537aa962
Simone Colazzo <colsimo88@gmail.com>
2020-03-08 18:21:27 +0100

    Restored and improved auth ar logics

--- src/pages/augmented-reality/augmented-reality.page.ts
+++ src/pages/augmented-reality/augmented-reality.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 
 import { select } from "@angular-redux/store";
 import { Observable, Subject } from "rxjs";
@@ -7,13 +7,17 @@ import { first, takeUntil, filter } from "rxjs/operators";
 import { StatusBar } from "@ionic-native/status-bar/ngx";
 import { ScreenOrientation } from "@ionic-native/screen-orientation/ngx";
 import { Diagnostic } from "@ionic-native/diagnostic/ngx";
+import { LocationAccuracy } from "@ionic-native/location-accuracy/ngx";
 import { NativeStorage } from "@ionic-native/native-storage/ngx";
+import { CameraPreview, CameraPreviewOptions } from "@ionic-native/camera-preview/ngx";
 
-import { SpinnerActions } from "../../store";
+import { SpinnerActions, ARActions, GpsActions } from "../../store";
 
 import { SensorsService } from "../../services/sensors.service";
 
 import { constants } from '../../utils/constants';
+
+import { FusionSensorsDTO } from "../../entities/dto/fusionSensorsDTO";
 
 enum ARError {
   INTERNAL_AR_ERROR = "INTERNAL_AR_ERROR",
@@ -30,10 +34,11 @@ enum ARError {
   templateUrl: './augmented-reality.page.html',
   styleUrls: ['./augmented-reality.page.scss'],
 })
-export class AugmentedRealityPage implements OnInit, AfterViewInit
+export class AugmentedRealityPage implements OnInit, AfterViewInit, OnDestroy
 {
   @select(["platformDevice", "infos", "platform"])
   os$: Observable<string>;
+  os: string;
 
   private cameraPresent: boolean;
   private cameraAuthorized: boolean;
@@ -45,23 +50,35 @@ export class AugmentedRealityPage implements OnInit, AfterViewInit
   private authFlagsRetrieve$: Subject<void>;
 
   @select(["accelerometer", "error"])
-  accelerometerCoordinatesError$: Observable<boolean>;
+  private accelerometerCoordinatesError$: Observable<boolean>;
   @select(["gyroscope", "error"])
-  gyroscopeCoordinatesError$: Observable<boolean>;
+  private gyroscopeCoordinatesError$: Observable<boolean>;
   @select(["magnetometer", "error"])
-  magnetometerCoordinatesError$: Observable<boolean>;
-  private sensorsErrors$: Subject<void>;
+  private magnetometerCoordinatesError$: Observable<boolean>;
+  private sensorsErrorsUnsubscribe$: Subject<void>;
+  private sensorsError: boolean;
+  private sensorsUnsubscribe$: Subject<void>;
+
+  @select(["ar", "spotArray"])
+  spotArray$: Observable<any[]>;
+  spotArray: any[] = [];
 
   constructor(
     private statusBar: StatusBar,
     private screenOrientation: ScreenOrientation,
     private diagnosticService: Diagnostic,
+    private locationAccuracy: LocationAccuracy,
     private nativeStorage: NativeStorage,
+    private cameraPreview: CameraPreview,
+    private gpsService: GpsActions,
     private spinnerActions: SpinnerActions,
-    private sensorsService: SensorsService
+    private sensorsService: SensorsService,
+    private arInfos: ARActions
   ) {
     this.authFlagsRetrieve$ = new Subject<void>();
-    this.sensorsErrors$ = new Subject<void>();
+    this.sensorsErrorsUnsubscribe$ = new Subject<void>();
+    this.sensorsUnsubscribe$ = new Subject<void>();
+    this.sensorsError = false;
   }
 
   async ngOnInit()
@@ -69,6 +86,8 @@ export class AugmentedRealityPage implements OnInit, AfterViewInit
     this.statusBar.hide();
     
     this.os$.pipe(first()).subscribe(os => {
+      this.os = os;
+
       if (os === 'ios')
         this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE_SECONDARY);
       else if (os === 'android')
@@ -113,8 +132,12 @@ export class AugmentedRealityPage implements OnInit, AfterViewInit
   {
     await this.authFlagsRetrieve$.toPromise();
 
+    //Something went wrong on system authorizations retrieve. Show alert and abort all.
     if (this.preloadAuthorizationError)
+    {
       this.manageARSystemsErrors(ARError.INTERNAL_AR_ERROR);
+      return;
+    }
 
     this.spinnerActions.showLoader();
 
@@ -125,40 +148,248 @@ export class AugmentedRealityPage implements OnInit, AfterViewInit
 
     this.accelerometerCoordinatesError$
       .pipe(
-        takeUntil(this.sensorsErrors$),
+        takeUntil(this.sensorsErrorsUnsubscribe$),
         takeUntil(this.gyroscopeCoordinatesError$),
         takeUntil(this.magnetometerCoordinatesError$),
         filter(data => data != null && data != undefined))
       .subscribe(flag => {
-        if (flag) this.manageARSystemsErrors(ARError.SENSORS_ERROR);
+        if (flag)
+        {
+          this.sensorsError = true;
+          this.manageARSystemsErrors(ARError.SENSORS_ERROR);
+        }
       });
 
     this.gyroscopeCoordinatesError$
       .pipe(
-        takeUntil(this.sensorsErrors$),
+        takeUntil(this.sensorsErrorsUnsubscribe$),
         takeUntil(this.accelerometerCoordinatesError$),
         takeUntil(this.magnetometerCoordinatesError$),
         filter(data => data != null && data != undefined))
       .subscribe(flag => {
-        if (flag) this.manageARSystemsErrors(ARError.SENSORS_ERROR);
+        if (flag)
+        {
+          this.sensorsError = true;
+          this.manageARSystemsErrors(ARError.SENSORS_ERROR);
+        }
       });
 
     this.magnetometerCoordinatesError$
       .pipe(
-        takeUntil(this.sensorsErrors$),
+        takeUntil(this.sensorsErrorsUnsubscribe$),
         takeUntil(this.accelerometerCoordinatesError$),
         takeUntil(this.magnetometerCoordinatesError$),
         filter(data => data != null && data != undefined))
       .subscribe(flag => {
-        if (flag) this.manageARSystemsErrors(ARError.SENSORS_ERROR);
+        if (flag)
+        {
+          this.sensorsError = true;
+          this.manageARSystemsErrors(ARError.SENSORS_ERROR);
+        }
       });
 
-    this.
+    setTimeout(async () => {
+      this.sensorsErrorsUnsubscribe$.next();
+      this.sensorsErrorsUnsubscribe$.complete();
+
+      if (this.sensorsError)
+        return;
+
+      if (!this.cameraPresent)
+      {
+        this.manageARSystemsErrors(ARError.CAMERA_SYSTEM_NOT_FOUND);
+        return;
+      }
+
+      if (!this.cameraAuthorized)
+      {
+        let cameraAuth = await this.diagnosticService.requestCameraAuthorization();
+        console.log("cameraAuth: ", cameraAuth);
+
+        if (cameraAuth.toLowerCase().indexOf("denied") >= 0)
+        {
+          this.manageARSystemsErrors(ARError.CAMERA_PERMISSION_NOT_GRANTED);
+          return;
+        }
+      }
+
+      //Here camera is present and is authorized. Start it anc continue to check/request other permissions
+      this.initCamera();
+
+      if (!this.locationEnabled)
+      {
+        this.manageARSystemsErrors(ARError.LOCATION_SERVICE_DISABLED);
+        return;
+      }
+
+      if (!this.locationAvailable || !this.locationAuthorized)
+      {
+        if (this.os === 'ios')
+        {
+          if (this.firstLocationAuthorization)  //iOS doesn't return anything if the permission is requested many times
+          {
+            let locAuth = await this.diagnosticService.requestLocationAuthorization();
+            console.log("locAuth: ", locAuth);
+  
+            this.nativeStorage.setItem(constants.FIRST_LOCATION_PERMISSION_REQUEST, true);
+    
+            if (locAuth.toLowerCase().indexOf("denied") >= 0)
+            {
+              this.manageARSystemsErrors(ARError.LOCATION_PERMISSION_NOT_GRANTED);
+              return;
+            }
+          }
+          else
+          {
+            this.manageARSystemsErrors(ARError.LOCATION_PERMISSION_NOT_GRANTED);
+            return;
+          }
+        }
+        else    //Android has no issues on request the same permission many times
+        {
+          let locAuth = await this.diagnosticService.requestLocationAuthorization();
+          console.log("locAuth: ", locAuth);
+  
+          if (locAuth.toLowerCase().indexOf("denied") >= 0)
+          {
+            this.manageARSystemsErrors(ARError.LOCATION_PERMISSION_NOT_GRANTED);
+            return;
+          }
+        }
+      }
+
+      //Here all permissions are granted
+      //Only on Android request max location precision
+      if (this.os === 'android')
+      {
+        let canRequest = await this.locationAccuracy.canRequest();
+        console.log("canRequest: ", canRequest);
+
+        if (canRequest)
+        {
+          this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
+            () => {
+              this.startARSystems();
+            },
+            error => {
+              this.manageARSystemsErrors(ARError.GPS_NOT_ENABLED);
+            }
+          );
+        }
+      }
+      else if (this.os === 'ios')
+      {
+        this.startARSystems();
+      }
+    }, 1500);
+  }
+
+  private initCamera()
+  {
+    let isIos = this.os === 'ios';
+    const cameraPreviewOpts: CameraPreviewOptions = {
+      x: 0,
+      y: 0,
+      width: isIos? window.screen.height : window.screen.width,
+      height: isIos? window.screen.width : window.screen.height,
+      camera: 'rear',
+      tapPhoto: false,
+      previewDrag: false,
+      toBack: true,
+      alpha: 1
+    };
+
+    // start camera
+    this.cameraPreview.startCamera(cameraPreviewOpts).then(res => {
+      console.log(res);
+  
+      //Initializing camera FOV used on AR calculations
+      this.cameraPreview.getHorizontalFOV().then((fov: number) => {
+        console.log("FOV: ", fov);
+        this.arInfos.setCameraFov(Math.abs(fov));
+      });
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  private startARSystems()
+  {
+    this.gpsService.startService();
+
+    //Retrieve sensors fusion coordinates and pass to redux ar infos (redux saga does the remaining calculations)
+    let sensorsFusionOptions = {
+      frequency: constants.FUSION_SENSOR_FREQUENCY,
+      delay: constants.FUSION_SENSOR_INIT_DELAY
+    };
+
+    this.sensorsService.getSensorsData(sensorsFusionOptions).pipe(takeUntil(this.sensorsUnsubscribe$))
+      .subscribe((fusionOrientation: FusionSensorsDTO) => {
+        if (fusionOrientation && fusionOrientation.alfa && fusionOrientation.beta && fusionOrientation.gamma)
+          this.arInfos.setFusionCoordinates(fusionOrientation);
+      });
+
+    //Hide spinner
+    setTimeout(() => this.spinnerActions.dismissLoader(), sensorsFusionOptions.delay);
+
+    this.spotArray$.pipe(takeUntil(this.sensorsUnsubscribe$)).subscribe((spotArray: any[]) => {
+      if (spotArray)
+      {
+        //Here we have to update current spot array, and add new spots (if there are)
+        //We cannot simply replace redux spots stored, because on iOS this produces a boring
+        //flickering effect on markers
+        for (let spot of spotArray)
+        {
+          let spotToEvaluate = null;
+  
+          //Search the spot in the page spot array
+          for (let i = 0; i < this.spotArray.length; ++i)
+            if (this.spotArray[i].id == spot.id)
+              spotToEvaluate = this.spotArray[i];
+  
+          if (!spotToEvaluate) //The spot doesn't exist. Let's add it
+          {
+            let spotToAdd = spot;
+            spotToAdd['onFire'] = true;   //Flag added to mark a new or modified spot
+            this.spotArray.push(spotToAdd);
+          }
+          else  //The spot exist; let's update its displacement informations
+          {
+            spotToEvaluate.screenRelativePositionX = spot.screenRelativePositionX;
+            spotToEvaluate.screenRelativePositionY = spot.screenRelativePositionY;
+            spotToEvaluate['onFire'] = true;  //Flag added to mark a new or modified spot
+          }
+        }
+  
+        //Finally we remove old spots, and set new/modified spots as evaluated (onFire to false)
+        for (let j = 0; j < this.spotArray.length; ++j)
+        {
+          if (this.spotArray[j]['onFire'])
+            this.spotArray[j]['onFire'] = false;
+          else
+            this.spotArray.splice(j, 1);
+        }
+      }
+    });
   }
 
   private manageARSystemsErrors(errorType: ARError)
   {
-    //this.spinnerService.dismissLoader();
+    this.spinnerActions.dismissLoader();
     //this.alertService.showSensorsError(errorType);
+  }
+
+  ngOnDestroy()
+  {
+    this.spinnerActions.dismissLoader();
+    this.statusBar.show();
+    this.sensorsService.stopSensors();
+    this.cameraPreview.stopCamera();
+    this.gpsService.stopService();
+    
+    setTimeout(() => this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT), 500);
+
+    this.sensorsUnsubscribe$.next();
+    this.sensorsUnsubscribe$.complete();
   }
 }
